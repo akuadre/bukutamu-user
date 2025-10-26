f2078f5bc30b47ea9c754af811fb47fa44d0f862
Adre <dreenation21@gmail.com>
2025-10-26 14:53:01 +0700

    Remove unused files and setup specific app

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,9 @@ import {
   Navigate,
   useLocation,
 } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import AppLayout from "./layouts/AppLayout.jsx";
 
-// Import komponen halaman baru sesuai sidebar
-import Dashboard from "./pages/Dashboard.jsx";
-import Siswa from "./pages/Siswa.jsx";
-import OrangTua from "./pages/OrangTua.jsx";
-import Jabatan from "./pages/Jabatan.jsx";
-import Pegawai from "./pages/Pegawai.jsx";
-import BukuTamu from "./pages/BukuTamu.jsx";
-import About from "./pages/About.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import GuestbookPage from "./pages/GuestbookPage.jsx";
-
-import { GuestRoute, ProtectedRoute } from "./routes/AuthRoutes.jsx";
 
 // BUAT COMPONENT TERPISAH UNTUK SCROLL TO TOP
 function ScrollToTop() {
@@ -82,39 +70,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
 
-        {/* Rute untuk halaman Login (Guest Only) */}
-        <Route element={<GuestRoute />}>
-          <Route path="/login" element={<Login />} />
-        </Route>
-
-        {/* Rute yang dilindungi, butuh login */}
-        <Route element={<ProtectedRoute />}>
-          <Route element={<AppLayout />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/siswa" element={<Siswa />} />
-            {/* <Route path="/orangtua" element={<OrangTua />} /> */}
-            {/* <Route path="/jabatan" element={<Jabatan />} /> */}
-            <Route path="/pegawai" element={<Pegawai />} />
-            <Route path="/bukutamu" element={<BukuTamu />} />
-            <Route path="/about" element={<About />} />
-          </Route>
-        </Route>
-
         {/* Rute input buku tamu */}
         <Route path="/input" element={<GuestbookPage />} />
 
-        {/* Jika user sudah login dan mengakses /login, redirect ke page lain */}
-        {/* <Route
-          path="/login"
-          element={
-            <ProtectedRoute>
-              <Navigate to="/" replace />
-            </ProtectedRoute>
-          }
-        /> */}
-
         {/* Jika rute tidak ditemukan, arahkan ke halaman utama */}
-        {/* <Route path="*" element={<Navigate to="/dashboard" replace />} /> */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
