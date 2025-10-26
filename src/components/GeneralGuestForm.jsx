fad36b3e661131ce785eb9a7392aa24fad9bd1cb
Adre <dreenation21@gmail.com>
2025-10-26 15:21:54 +0700

    feat: connect url with env

--- src/components/GeneralGuestForm.jsx
+++ src/components/GeneralGuestForm.jsx
@@ -13,7 +13,7 @@ import { InputField, SelectField } from "./InputField";
 import WebcamCapture from "./WebcamCapture";
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 
 const GeneralGuestForm = () => {
   const navigate = useNavigate();
